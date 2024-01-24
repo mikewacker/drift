1eb86ace1452ddae50b92298fd2250ce97bf1872
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2024-01-24 01:56:40 -0700

    don't publish test fixtures to Maven Central (#48)

--- drift/build.gradle.kts
+++ drift/build.gradle.kts
@@ -39,3 +39,8 @@ publishing {
         }
     }
 }
+
+// Don't publish test fixtures.
+val javaComponent = components["java"] as AdhocComponentWithVariants
+javaComponent.withVariantsFromConfiguration(configurations["testFixturesApiElements"]) { skip() }
+javaComponent.withVariantsFromConfiguration(configurations["testFixturesRuntimeElements"]) { skip() }
